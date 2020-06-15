0082a12e029d3be45676d87b94893dc25fc66e05
JeongYeong Park <pjy4952@gmail.com>
2020-06-15 15:05:59 +0900

    정영 - 파일 업로드 삭제

--- src/main/java/com/fivekm_home_charge/charge/ChargeApplication.java
+++ src/main/java/com/fivekm_home_charge/charge/ChargeApplication.java
@@ -1,7 +1,5 @@
 package com.fivekm_home_charge.charge;
 
-import com.fivekm_home_charge.charge.config.StorageConfig;
-import com.fivekm_home_charge.charge.service.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,19 +7,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageConfig.class)
 public class ChargeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChargeApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner init(StorageService storageService){
-        return (args -> {
-            storageService.deleteAll();
-            storageService.init();
-        });
     }
 
 }
