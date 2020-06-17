66a5c963358618f00b3fbe61053b2e4a55156674
JeongYeong Park <pjy4952@gmail.com>
2020-06-18 02:09:59 +0900

    주차장 리스트에 db불러오기 완료(지도랑 라디오버튼 추가해야함)2

--- src/main/resources/static/js/happyParking.js
+++ src/main/resources/static/js/happyParking.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
     var data = {
         parkingType : $('#parkingType').val(),
-        wrap : function () {
-            return function (text) {
-                return text.replace('value="' + this.parkingType+'"', 'value="' + this.parkingType+'" checked');
-            }
+        parkingTypeDefault1 : function () {
+            return this.parkingType == '단독주택';
+        },
+        parkingTypeDefault2 : function () {
+            return this.parkingType == '개인 사유지';
+        },
+        parkingTypeDefault3 : function () {
+            return this.parkingType == '공동 주택';
+        },
+        parkingTypeDefault4 : function () {
+            return this.parkingType == '기타';
         }
     };
 
+    var template = $("#tmpl").html();
+    var result = Mustache.render(template, data);
+
+    $("#formWrapper").html(result);
 
 });
